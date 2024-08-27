6d0c61abe8ad10a4db5c2691f5cacda2ad22e9b9
tacua <tiago24r@gmail.com>
2024-08-26 20:48:19 -0600

    Commit1

--- app/config/env.js
+++ app/config/env.js
@@ -1,21 +1,23 @@
 const env = {
-    database: process.env.database|'apis_re0f', // Nombre de la base de datos
-    user: 'apis_re0f_user', // Usuario de la base de datos
-    password: '4ryV3j9FP4aEbJqXKjmzEV68nW4FjDva', // Contraseña del usuario
-    host: 'dpg-cr6fffbqf0us73alch70-a.oregon-postgres.render.com', // Host del servidor
-    dialect: 'postgres', // Dialecto de la base de datos
-    pool: {
-        max: 5, // Máximo de conexiones en el pool
-        min: 0, // Mínimo de conexiones en el pool
-        acquire: 30000, // Tiempo máximo, en milisegundos, que el pool intentará obtener una conexión antes de lanzar un error
-        idle: 10000 // Tiempo máximo, en milisegundos, que una conexión puede estar inactiva antes de ser liberada
-    },
+    database: 'apis_qnhe',
+    user: 'apis_qnhe_user',
+    password: '4P4N5mtaW1sOEo8ySSOk5NOzZgeinjhg',
+    host: 'dpg-cr6jrqlds78s73butrqg-a.oregon-postgres.render.com',
+    dialect: 'postgres',
+    port: 5432,
     dialectOptions: {
-        ssl: {
-            require: true, // Necesita SSL
-            rejectUnauthorized: false // Permitir certificados no autorizados
-        }
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     }
-};
-
-module.exports = env;
+  };
+  
+  module.exports = env;
+  
